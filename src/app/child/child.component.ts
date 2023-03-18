422a684ac45b38bed81896ffc2c550688364a2fc
ajeet rajbhar <ajeetrajbhar2504@gmail.com>
2023-03-19 01:23:09 +0530

    scroll by mouse also

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,12 +1,38 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.scss']
 })
-export class ChildComponent {
+export class ChildComponent implements OnInit {
 
+   isDragging = false;
+   startX:any;
+   scrollLeft:any;
+
+  ngOnInit(): void {
+    const slider:any = document.getElementById('scrollContainer')
+ 
+    
+    slider.addEventListener('mousedown', (e:any) => {
+      this.isDragging = true;
+      this.startX = e.pageX - slider.offsetLeft;
+      this.scrollLeft = slider.scrollLeft;
+    });
+    
+    slider.addEventListener('mousemove', (e:any) => {
+      if (!this.isDragging) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - this.startX) * 1; // adjust scrolling speed here
+      slider.scrollLeft = this.scrollLeft - walk;
+    });
+    
+    slider.addEventListener('mouseup', () => {
+      this.isDragging = false;
+    });
+  }
 
   scrollnextFunction() {
     const scrollContainer:any = document.getElementById("scrollContainer");
